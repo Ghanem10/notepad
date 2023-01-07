d481a4252258b3d5f7d3ca32a4a5c83b5472d4be
Gani <ghanemalhamad10@gmail.com>
2023-01-07 13:38:44 +0300

    work

--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,15 @@
 
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const mongoClient = require("mongodb").MongoClient;
+mongoClient.connect("mongodb://taskmanagermongodb:pS4YkdzPLiDEcMpEy6Fu5WiI639aHWcMJkuNcDh0OCKjdazcwHDCjg9kIDZZ8Q0qproq9bGNZfoBACDbTPxi4g%3D%3D@taskmanagermongodb.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@taskmanagermongodb@", function (err, client) {
+  client.close();
+});
+
+
 
 mongoose.set('strictQuery', false);
 const connectDB = (url) => {
