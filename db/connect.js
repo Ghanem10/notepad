eeeb774a015466b9e1d1f5148e523653c827d361
Gani <ghanemalhamad10@gmail.com>
2023-01-07 13:53:04 +0300

    added string connect to mongoDB

--- db/connect.js
+++ db/connect.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 dotenv.config();
 
+import { MongoClient } from 'mongodb';
 
-const mongoClient = require("mongodb").MongoClient;
-mongoClient.connect("mongodb://taskmanagermongodb:pS4YkdzPLiDEcMpEy6Fu5WiI639aHWcMJkuNcDh0OCKjdazcwHDCjg9kIDZZ8Q0qproq9bGNZfoBACDbTPxi4g%3D%3D@taskmanagermongodb.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@taskmanagermongodb@", function (err, client) {
+MongoClient.connect("mongodb://taskmanagermongodb:pS4YkdzPLiDEcMpEy6Fu5WiI639aHWcMJkuNcDh0OCKjdazcwHDCjg9kIDZZ8Q0qproq9bGNZfoBACDbTPxi4g%3D%3D@taskmanagermongodb.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@taskmanagermongodb@", function (err, client) {
   client.close();
 });
 
